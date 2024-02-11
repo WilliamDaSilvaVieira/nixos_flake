50737cd33d91f3f402b8672a03e97efc47ead2a6
William <william_silva2003@hotmail.com>
2024-02-10 21:17:08 -0300

    flake added

---
+++ overlays/default.nix
@@ -0,0 +1,12 @@
+# This file defines overlays
+{ inputs, ... }:
+{
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.stable'
+  stable-packages = final: _prev: {
+    stable = import inputs.nixpkgs-stable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+}
