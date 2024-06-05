2da511c4f7f2d372300e2f7ab97c675a951625dc
William <william_silva2003@hotmail.com>
2024-06-04 21:09:39 -0300

    update

--- configuration.nix
+++ configuration.nix
@@ -105,21 +105,21 @@
     colors = [
       # Use terminal.sexy with alacritty
       # Material
-      "263238"
-      "ff9800"
-      "8bc34a"
-      "ffc107"
-      "03a9f4"
+      "192024"
+      "7b0404"
+      "099923"
+      "0ca9c6"
+      "218354"
       "e91e63"
-      "009688"
+      "e2bf06"
       "cfd8dc"
-      "37474f"
-      "ffa74d"
-      "9ccc65"
-      "ffa000"
+      "3f5b69"
+      "ef4848"
+      "045d9e"
+      "25a958"
       "81d4fa"
       "ad1457"
-      "26a69a"
+      "701b78"
       "eceff1"
     ];
   };
