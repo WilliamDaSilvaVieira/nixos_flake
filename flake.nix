6781733bf2b8fcd61e401e3c72ca9a55e3f5d1d7
William <william_silva2003@hotmail.com>
2024-12-17 00:15:06 -0300

    updated config

--- flake.nix
+++ flake.nix
@@ -9,15 +9,19 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # Nixpkgs-f2k
     nixpkgs-f2k.url = "github:moni-dz/nixpkgs-f2k";
+
     #Hyprland
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+
     #Cosmic
     # nixos-cosmic = {
     #   url = "github:lilyinstarlight/nixos-cosmic";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
+
     #Zen-Browser
     zen-browser.url = "github:ch4og/zen-browser-flake";
   };
@@ -33,30 +37,43 @@
     }@inputs:
     let
       inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
     in
     {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        import ./pkgs { inherit pkgs; }
+      );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        import ./shell.nix { inherit pkgs; }
+      );
       overlays = import ./overlays { inherit inputs; };
       nixosConfigurations = {
-        william = lib.nixosSystem {
+        william = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           inherit system;
           modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
+            ./nixos/configuration.nix
+            ./home_manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.william = {
-                imports = [ ./home.nix ];
-              };
               # nix.settings = {
               #   substituters =  ["https://cosmic.cachix.org/"];
               #   trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRe102smYzA85dPE=" ];
